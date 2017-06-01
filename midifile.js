877f49b085f0f7754c988334aea91c7b365f7c22
jlhelwig <jeff.helwig@gmail.com>
2017-06-01 15:29:38 -0500

    should set the timer function

--- midifile.js
+++ midifile.js
@@ -26,52 +26,109 @@
 
 
 $(document).ready(function(){
-  tempo1 =[];
+  var tempo1 =[];
+  var j=0
   // var JZZ = require('jzz');
   var midiObject ={};
   console.log("this is working");
+var columns = [".first_col",".second_col",".third_col",".fourth_col",".fifth_col",".sixth_col",".seventh_col",".eighth_col"]
+function colorMeGrey(){
+console.log('this should color the grid one time');
+  $(".first_col").addClass('silver');
+  $(".second_col").addClass('silver');
+  $(".third_col").addClass('silver');
+  $(".fourth_col").addClass('silver');
+  $(".fifth_col").addClass('silver');
+  $(".sixth_col").addClass('silver');
+  $(".seventh_col").addClass('silver');
+  $(".eighth_col").addClass('silver');
+}
+
+function myTimer(){
+  let i=0;
+
+  loopIt()
+  function loopIt(num){
+      setTimeout(function(){
+        // console.log('this first timeout is running');
+        $(columns[num]).addClass('blue')
+        setTimeout(function(){
+          // console.log('this second timeout is running');
+          $(columns[num]).removeClass('blue');
+        },200)
+
+        if (i <columns.length){
+          loopIt(i)
+          i+=1;
+          if (i===8){
+            myTimer()
+          }
+
+          // console.log('i ',i);
+        }
+      },200)
+    }
+    // }
+  }
 
 
-  $(".second_col").addClass('green');
-  $(".third_col").addClass('purple');
-  $(".fourth_col").addClass('orange');
-  $('.fifth_col').addClass('red');
-  $(".sixth_col").addClass('green');
-  $(".seventh_col").addClass('purple');
-  $(".eighth_col").addClass('orange');
 
 
+
+
+
+
+
+
+
+
+  //   function myTimer() {
+  //       function
+  //     for (let j=0; j<1;j){
+  //   $(".first_col").addClass('white')
+  //   setTimeout(function(){$('.first_col').addClass('silver'); },250)
+  //   j++
+  //   }
+  //
+  //
+  // }
+
+
+
+  // var synth = JZZ.synth.MIDIjs({ soundfontUrl: "soundfont/synth_drum-mp3", instrument: "synth_drum" })
+  //   .or(function(){ loaded(); alert('Cannot load MIDI.js!\n' + this.err()); })
+  //   .and(function(){ loaded(); });
   // JZZ.synth.OSC.register('Synth');
-  var port = JZZ().openMidiOut(['SimpleSynth','Apple DLS Synth',0]).or(function(){alert('Cannot open MIDI port!');});
+  var port = JZZ().openMidiOut().or(function(){alert('Cannot open MIDI port!');});
   console.log('port.info', port.info());
   // port.send([0x90,60,100]).send([0x90,64,100]).send([0x90,67,100])
   JZZ().openMidiIn().or('MIDI-In:  Cannot open!')
        .and(function(){ console.log('MIDI-In: ', this.name())})
        .connect(function(msg){
-        //  console.log(msg)
+
        midiObject = msg;
-      //  console.log('typeof msg is midiObject._from[0]._ready: ', typeof midiObject._from[0]._handles[0]);
-      //  console.log('msg is midiObject._from[0]._ready: ', midiObject._from[0]._info);
 
-      if (midiObject[0]==145) {
-        $(".first_col").toggleClass('red');
+      if (midiObject[0]===145) {
+          if (j===0){
+            colorMeGrey()
+            myTimer()
+          j++
+          }
 
-        // console.log('this is the midi note Note ON: ', midiObject[1]);
-        // console.log('port information', port.send([0x90,midiObject[1],127]))
+
+
+
+
         port.send([0x90,midiObject[1],127])
-        // var tempo = new Date()
-        // tempo1.push(tempo)
-        // console.log('tempo',tempo1);
-        // tempoDiff = tempo1[2]-tempo1[1]
-        // console.log('this checks difference in dates', tempoDiff);
+
       }
       if (midiObject[0]==129){
-        // console.log('this is the midi note Note OFF: ', midiObject[1]);
+
         port.send([0x80,midiObject[1],0])
-        $(".first_col").toggleClass('red');
+
       }
 
-      //  console.log('type of thing ',typeof midiObject[1]);
+
     })
        .wait(10000).close();
 
